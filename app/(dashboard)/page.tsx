6b54bbeb4215ab942182d5df109ba9bc9eeddc3f
garrettomi <omigarrett@gmail.com>
2024-07-19 14:10:43 +0900

    Feat: Add direct routing to individual books from dashboard access

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 import AddBookForm from "./form";
+import Link from "next/link";
 import { Book } from "../globals";
 
 async function getBooks() {
@@ -25,16 +26,18 @@ export async function Dashboard () {
             <AddBookForm />
             {books.map((book: Book) => (
                 <div key={book.id}>
-                    <img src={book.thumbnailUrl} alt={book.title}></img>
-                    <h3>{book.title}</h3>
-                    <h4>
-                        {book.authors.map((name: string, index: number) => (
-                            <span key={index}>
-                                {name}{index < book.authors.length - 1 && ', '}
-                            </span>
-                        ))}
-                    </h4>
-                    <h6>{book.publishedDate}</h6>
+                    <Link href={`/book/${book.id}`}>
+                        <img src={book.thumbnailUrl} alt={book.title}></img>
+                        <h3>{book.title}</h3>
+                        <h4>
+                            {book.authors.map((name: string, index: number) => (
+                                <span key={index}>
+                                    {name}{index < book.authors.length - 1 && ', '}
+                                </span>
+                            ))}
+                        </h4>
+                        <h6>{book.publishedDate}</h6>
+                    </Link>
                 </div>
             ))
             }
