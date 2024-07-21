2e860c8a753d4479d0b58c78c1428533ebfbdc7f
garrettomi <omigarrett@gmail.com>
2024-07-21 20:15:17 +0900

    Feat: Implement basic styling for individual book pages

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -21,23 +21,25 @@ export default async function Page ({ params }: { params: { id: string }}) {
     const bookInformation = await getBookById(params.id);
 
     return (
-        <div>
-            Book by id
-                <div>
-                    <img src={bookInformation.thumbnailUrl} alt={bookInformation.title}></img>
-                    <h3>{bookInformation.title}</h3>
-                    <h4>
+        <div className="bg-background min-h-screen p-6">
+            <h1 className="text-primary text-3xl font-semibold mb-4">
+            {bookInformation.title}
+            </h1>
+                <div className="bg-white rounded-lg shadow-md p-4">
+                    <img className="w-56 h-auto rounded mb-4" src={bookInformation.thumbnailUrl} alt={bookInformation.title}></img>
+                    <h3 className="text-textPrimary text-xl font-medium mb-2">{bookInformation.title}</h3>
+                    <h4 className="text-textSecondary text-lg mb-2">
                         {bookInformation.authors.map((name: string, index: number) => (
                             <span key={index}>
                                 {name}{index < bookInformation.authors.length - 1 && ', '}
                             </span>
                         ))}
                     </h4>
-                    <h6>{bookInformation.publishedDate}</h6>
-                    <p>{bookInformation.longDescription}</p>
+                    <h6 className="text-textSecondary text-sm mb-4">{bookInformation.publishedDate}</h6>
+                    <p className="text-textPrimary text-base mb-4">{bookInformation.longDescription}</p>
                 </div>
-                <Link href={`/edit/${params.id}`}>
-                Edit
+                <Link className="text-secondary text-sm font-semibold" href={`/edit/${params.id}`}>
+                    Edit
                 </Link>
         </div>
     );
