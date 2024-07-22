7c9f3cf5aaf8af0ee0c789fe599b0a0fd1237176
garrettomi <omigarrett@gmail.com>
2024-07-22 12:57:33 +0900

    Feat: Implement loading skeleton for dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,20 +2,23 @@
 
 import { useBooks } from '@/context';
 import Link from 'next/link';
+import { Suspense } from 'react';
+import Loading from './loading';
 import AddBooksForm from './add-book-form/add-book-form';
 import { formatDate } from '@/utils/formatDate';
 
 export function Dashboard() {
     const { books, error } = useBooks();
-    
+
     if (error) return <div>Error: {error}</div>;
 
     return (
         <div className="bg-background min-h-screen p-6">
             <h1 className="text-primary text-3xl front-semibold mb-4">
-            Dashboard
+            Books by Books
             </h1>
             <AddBooksForm />
+            <Suspense fallback={<Loading />}>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
                 {books.map((book) => (
                     <div key={book.id} className="bg-white rounded-lg shadow-md p-2">
@@ -34,6 +37,7 @@ export function Dashboard() {
                     </div>
                 ))}
             </div>
+            </Suspense>
         </div>
     );
 }
