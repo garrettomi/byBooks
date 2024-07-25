14843a0dc3eafe1b322577313c0a4a3c11b4352a
garrettomi <omigarrett@gmail.com>
2024-07-25 12:21:07 +0900

    Fix: Reinvoke metadata in root layout for app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { BooksProvider } from "@/context";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "byFood - Take Home",
-//   description: "Take home assessment for byFood",
-// };
+export const metadata: Metadata = {
+  title: "byFood - Take Home",
+  description: "Take home assessment for byFood",
+};
 
 export default function RootLayout({
   children,
