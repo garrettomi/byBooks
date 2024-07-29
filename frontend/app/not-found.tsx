b2457a81c7a62c5f69f2e5669573ce337ac7fb25
garrettomi <omigarrett@gmail.com>
2024-07-29 10:51:29 +0900

    Fix: Add base styling for not found page

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -3,12 +3,16 @@ import Link from "next/link";
 export default async function NotFound() {
     return (
         <>
-            <h2>Whoops, seems like this page can't be found</h2>
-            <button>
-                <Link href="/">
-                Go back to home
-                </Link>
-            </button>
+        <div className="flex flex-col items-center mt-20">
+            <h2 className="text-primary text-4xl text-center">Whoops, seems like this page can't be found</h2>
+            <div className="mt-10">
+                <button className="p-2 bg-secondary text-white rounded-full">
+                    <Link href="/">
+                    Go back to home
+                    </Link>
+                </button>
+            </div>
+        </div>
         </>
     )
 }
